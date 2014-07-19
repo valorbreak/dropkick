91a0109e816cd2639a8c8bc6579c39fea99ec497
Uri Acuesta <acuestal@hhmi.org>
2014-07-19 01:32:26 -0400

    app.go

--- app.go
+++ app.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-//	"html/template"
-	"log"
-	"github.com/valorbreak/dropkick/model"
+	//	"html/template"
 	"github.com/valorbreak/dropkick/core"
+	"github.com/valorbreak/dropkick/model"
 	"github.com/valorbreak/dropkick/routes"
+	"log"
 )
 
 // this is the main function
@@ -15,24 +15,24 @@ import (
 func main() {
 	fmt.Println("Hello, test")
 	port := ":8080"
-	
+
 	x := model.Bad()
 	core.Execute()
 	routes.Execute()
-	
+
 	fmt.Println(x)
-	log.Printf("About to listen on "+port)
+	log.Printf("About to listen on " + port)
 
 	welcome := routes.WelcomeHandler
 	view := routes.ViewHandler
 	json := routes.JsonHandler
 	http.HandleFunc("/foo", view)
 	http.HandleFunc("/", welcome)
-	http.HandleFunc("/api", json);
+	http.HandleFunc("/api", json)
 
 	// Start Listening to port
 	err := http.ListenAndServe(port, nil)
-	if err != nil{
+	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
