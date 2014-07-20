2970b6e244f1ada4c9933429da8fa575213a1163
Uri Acuesta <acuestal@hhmi.org>
2014-07-20 19:17:12 -0400

    updates to handler.go + routes.go

--- routes/handler.go
+++ routes/handler.go
@@ -3,13 +3,16 @@ package routes
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request){
+	log.Printf("Accessed: /%s", r.URL.Path[1:])
 	fmt.Fprintf(w,"Welcome %s", r.URL.Path[1:])
 }
 
 func ViewHandler(w http.ResponseWriter, r *http.Request){
+	log.Printf("Accessed: /%s", r.URL.Path[1:])
 	fmt.Fprintf(w,"<h1>Hi there, I love %s</h1>", r.URL.Path[1:])
 }
 
@@ -17,6 +20,6 @@ func JsonHandler(w http.ResponseWriter, r *http.Request){
 	const jsonStream = `
 		{"json":"test"}
 	`
-
+	log.Printf("Accessed: /%s", r.URL.Path[1:])
 	fmt.Fprint(w,jsonStream)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"flag"
 )
-
 
 func AppStart(port string) error{
 	// Logging
 	fmt.Printf("Hey Route is working\n")
 	
 	// Set the Urls here
-	http.HandleFunc("/", WelcomeHandler)
-	http.HandleFunc("/json", JsonHandler)
+	dir := flag.String("directory", "web/", "directory of files")
+	flag.Parse()
+
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+
+	http.Handle("/",fileHandler)
+	http.Handle("/static/", fileHandler)
+	
+	//http.HandleFunc("/", WelcomeHandler)
 	http.HandleFunc("/view", ViewHandler)
+	http.HandleFunc("/json", JsonHandler)
+	http.HandleFunc("/new", JsonHandler)
+	http.HandleFunc("/new/", JsonHandler)
+	
 	log.Printf("/, /json, /view is available")
+	
 	// Start Listening on port
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
